a66972c49c9321443137590f5639b6b3b1e1d762
papunkumar101 <papunmahakul1999@gmail.com>
2020-09-18 08:16:11 +0530

    initial commit

---
+++ init.js
@@ -0,0 +1,21 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+$('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true,
+        duration:2
+        
+      });
+ $(document).ready(function(){
+    $('.fixed-action-btn').floatingActionButton();
+  });
+        
+   
+   
+   
